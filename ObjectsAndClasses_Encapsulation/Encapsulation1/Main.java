4c19aa4fab7d868f65b07a2dc76941e94bf5db2a
sergei_kashirov <sebopero@gmail.com>
2022-10-25 20:11:57 +0300

    elevator created

--- ObjectsAndClasses_Encapsulation/Encapsulation1/Main.java
+++ ObjectsAndClasses_Encapsulation/Encapsulation1/Main.java
@@ -1,11 +1,12 @@
-package Encapsulation1;
-public class Main {
-        public static void main(String[] args) {
-                Tank tank = new Tank("т34", 30, false);
-                tank.setName("t50");
-                tank.setMaxSpeed(50);
-                tank.setSwim(true);
-                System.out.println("Танк: " + tank.getName() + "\n" + "Скорость: " + tank.getMaxSpeed() + "\n" + "Плавает: " + tank.isSwim());
-        }
-}
 
+public class Main {
+    public static void main(String[] args) {
+
+        Elevator elevator = new Elevator(-3, 26);
+        while (true) {
+            System.out.print("Введите номер этажа: ");
+            int floor = new Scanner(System.in).nextInt();
+            elevator.move(floor);
+        }
+    }
+}
