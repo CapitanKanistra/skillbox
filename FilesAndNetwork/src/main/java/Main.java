5d6ab70688e79c777c928fedc830a895ea61dea3
sergei_kashirov <sebopero@gmail.com>
2023-01-08 16:22:50 +0300

    all work

--- FilesAndNetwork/src/main/java/Main.java
+++ FilesAndNetwork/src/main/java/Main.java
@@ -2,6 +2,7 @@ import FileParser.FileParser;
 import FileParser.Stations;
 import MoscowUndergroundParser.ParseMoscowUnderground;
 import MoscowUndergroundParser.Underground;
+import SearchFiles.SearchFileInFolders;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -11,11 +12,13 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) {
+        String path = "D:/data/";
 
         String[] JSONPath = {"D:/data/2/4/depths-1.json", "D:/data/4/6/depths-3.json", "D:/data/7/1/depths-2.json"};
         String[] CSVPath = {"D:/data/0/5/dates-2.csv", "D:/data/4/6/dates-1.csv", "D:/data/9/6/dates-3.csv"};
 
         try {
+            SearchFileInFolders searchFileInFolders = new SearchFileInFolders(path);
 
             ParseMoscowUnderground parseMoscowUnderground = new ParseMoscowUnderground();
             FileParser fileParser = new FileParser();
---
+++ FilesAndNetwork/src/main/java/SearchFiles/SearchFileInFolders.java
@@ -0,0 +1,36 @@
+package SearchFiles;
+
+import java.io.File;
+import java.util.Objects;
+
+public class SearchFileInFolders {
+
+    public SearchFileInFolders(String path){
+        searchFile(path);
+    }
+
+    private void searchFile(String path){
+
+        File file = new File(path);
+
+        if (file.isDirectory()){
+            scanFolder(file);
+        } else {
+            System.out.println("Ошибка! Данный путь не является директорией!");
+        }
+
+    }
+
+    private void scanFolder(File file){
+        if (file.isFile()){
+            System.out.println(file.getName() + " -> " + file.getPath());
+        }
+
+        if (file.isDirectory()){
+            File[] folder = file.listFiles();
+            for (File files : Objects.requireNonNull(folder)){
+                scanFolder(files);
+            }
+        }
+    }
+}
