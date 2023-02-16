8945f2108f187599afcf7065bdd76b62cbefe061
sergei_kashirov <sebopero@gmail.com>
2023-02-16 19:09:45 +0300

    Lesson 4 done

--- ToDoList/src/main/java/main/DefaultController.java
+++ ToDoList/src/main/java/main/DefaultController.java
@@ -1,16 +1,32 @@
 package main;
 
+import main.model.Course;
+import main.model.TodolistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
 
-@RestController
+@Controller
 
 public class DefaultController {
+    @Autowired
+    TodolistRepository todolistRepository;
     @RequestMapping("/")
-    public String index()
+    public String index(Model model)
     {
-        return (new Date()).toString();
+        Iterable<Course> courseIterable = todolistRepository.findAll();
+        ArrayList<Course> courses = new ArrayList<>();
+        for (Course course:courseIterable){
+            courses.add(course);
+        }
+        model.addAttribute("courses",courses);
+        model.addAttribute("CoursesCount",courses.size());
+
+        return "index";
     }
 }
